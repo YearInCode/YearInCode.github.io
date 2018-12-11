076929df86f36d59da452215a3526b5fc73da3a6
Rishabh Anand <mail.rishabh.anand@gmail.com>
2018-12-11 23:42:50 +0800

    Added Request

--- app.js
+++ app.js
@@ -23,11 +23,10 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  xhr.open("GET", "https://github.com/login/oauth/authorize");
-  // xhr.setRequestHeader(
-  //   "cookie",
-  //   "has_recent_activity=1; ignored_unsupported_browser_notice=false; _gh_sess=b0JBTEF1RXcyaGhPVGp5dzJCYkMvbXlqREx6QnFQbjBkRDU3UTNHNjZ3ZGdBd01xVFYySm5IOHZ4SWxMU2QzcFFHUEhqVmFlVXJpKzJ5MDZkMU45cUIyRDhLUW9JT2ZFeExQcnljcDdieE95V3FsdmlqbEd5TXpMb25uQVlJc1lGSTJMMkpvYWhuWE5XNk1oc3VhUmFtcUlvaDRqd1RlTlJCY002ZWwxbGRBTkNLeXRmQ2tSZjN4RWtnaWpsdHFQVUZibGp5T0ZOTlFJUkVwTVRDdmsvZXRKTmVBc1dUUWpkK1pTdVhUWjNWWGpKWHJWQXdaR0NTZGppMFhwWUN2SXMzNit2V2Jta3ZUZ1M2UExJR3FiWndoTU1uUnZvUXJ2Q0x2eW0yNlFKc0dhaHFMS1VCN3NIZjRRN2lVZ1MvajNRV1YyK0ViMGlvZ0ptYVF2TVdNTzRZNWtseVo1T3NFYS90R1FlTlQ0bVhnPS0tb1lpQXBBZDU5SmNzcnVnWThBVEEvZz09--d4257ab65d6a7b85a1f0df104c087335c56ad885; logged_in=no"
-  // );
+  xhr.open(
+    "GET",
+    `https://github.com/login/oauth/authorize?client_id=${clientID}&client_secret=${clientSecret}`
+  );
 
   xhr.send(data);
 });
