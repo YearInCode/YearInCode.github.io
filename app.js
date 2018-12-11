7df800b46d245b135007746973d4613e7cb4ebe6
Rishabh Anand <mail.rishabh.anand@gmail.com>
2018-12-12 00:19:22 +0800

    Added OAuth

--- app.js
+++ app.js
@@ -5,28 +5,29 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const clientID = "05d4f56f89ba1a91c3bd";
   const clientSecret = "a9e53f438683513f1fd92d561cce3252af0504e9";
+  const authorizationBaseUrl = "https://github.com/login/oauth/authorize";
+  const tokenUrl = "https://github.com/login/oauth/access_token";
 
-  authButton.onclick = () => {
-    // Authenticate GitHub users using OAuth
-    console.log("Authenticating user...");
-    window.location = "display.html";
-  };
-
-  var data = null;
-
-  var xhr = new XMLHttpRequest();
-  xhr.withCredentials = true;
-
-  xhr.addEventListener("readystatechange", function() {
-    if (this.readyState === this.DONE) {
-      console.log(this.responseText);
-    }
+  $("#auth-button").on("click", function() {
+    // Initialize with your OAuth.io app public key
+    OAuth.initialize("h1d3NDcSh7Eflhg7EOnyKHm2eXU");
+    // Use popup for oauth
+    // Alternative is redirect
+    OAuth.popup("github").then(github => {
+      console.log("github:", github);
+      // Retrieves user data from oauth provider
+      // Prompts 'welcome' message with User's email on successful login
+      // #me() is a convenient method to retrieve user data without requiring you
+      // to know which OAuth provider url to call
+      github.me().then(data => {
+        console.log("me data:", data);
+        window.alert("GitHub says your email is:" + data.email);
+      });
+      // Retrieves user data from OAuth provider by using #get() and
+      // OAuth provider url
+      github.get("/user").then(data => {
+        console.log("self data:", data);
+      });
+    });
   });
-
-  xhr.open(
-    "GET",
-    `https://github.com/login/oauth/authorize?client_id=${clientID}&client_secret=${clientSecret}`
-  );
-
-  xhr.send(data);
 });
