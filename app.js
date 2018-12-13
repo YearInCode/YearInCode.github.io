323a906a517033cc39653f0a6a2251a24d0871cd
Rishabh Anand <mail.rishabh.anand@gmail.com>
2018-12-13 19:55:04 +0800

    Added firebase integration

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+var config = {
+  apiKey: "AIzaSyC_ALjoUddvJVfVbKsWlD-FFK-jtyrg4PQ",
+  authDomain: "wrapped-21a29.firebaseapp.com",
+  databaseURL: "https://wrapped-21a29.firebaseio.com",
+  projectId: "wrapped-21a29",
+  storageBucket: "wrapped-21a29.appspot.com",
+  messagingSenderId: "606608835269"
+};
+firebase.initializeApp(config);
+
 document.addEventListener("DOMContentLoaded", () => {
   console.log("App loaded!");
 
@@ -5,18 +15,35 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const clientID = "05d4f56f89ba1a91c3bd";
   const clientSecret = "a9e53f438683513f1fd92d561cce3252af0504e9";
-  const authorizationBaseUrl = "https://github.com/login/oauth/authorize";
-  const tokenUrl = "https://github.com/login/oauth/access_token";
 
-  $("#auth-button").on("click", function() {
-    // Initialize with your OAuth.io app public key
-    OAuth.initialize("h1d3NDcSh7Eflhg7EOnyKHm2eXU");
-    // Use popup for oauth
-    // Alternative is redirect
-    OAuth.popup("github").then(github => {
-      console.log("github:", github);
-      var w = window.open("./display.html");
-      w.accessToken = github;
-    });
+  var provider = new firebase.auth.GithubAuthProvider();
+  provider.addScope("repo");
+  provider.addScope("user");
+  provider.setCustomParameters({
+    allow_signup: true
   });
+
+  authButton.onclick = () => {
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then(function(result) {
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        console.log(token, user);
+      })
+      .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+  };
 });
