11cdd7f8dafbfdb480a322fd4cb394a45a1d90da
malhi3 <malharthebestfriend@gmail.com>
2018-12-29 23:11:10 +0800

    config file

---
+++ config.js
@@ -0,0 +1,11 @@
+var config = {
+  apiKey: "AIzaSyDnY5vGhGekMtd_JZqXxRZp8CVJ4Exaj_E",
+  authDomain: "yearincode-b03f4.firebaseapp.com",
+  databaseURL: "https://yearincode-b03f4.firebaseio.com",
+  projectId: "yearincode-b03f4",
+  storageBucket: "",
+  messagingSenderId: "809927327037"
+};
+
+const clientIDConfig = "05d4f56f89ba1a91c3bd";
+const clientSecretConfig = "a9e53f438683513f1fd92d561cce3252af0504e9";
